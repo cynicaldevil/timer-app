45911551b2e410bb03f4ec20d5402823145a97a1
Nikhil <nikhilshagri@gmail.com>
2015-11-30 19:21:37 +0530

    added stop button for countdown

--- cdown.js
+++ cdown.js
@@ -5,16 +5,50 @@ ctdwntimer.controller('ctdwnController',['$scope','$timeout','$interval',functio
     $scope.seconds=0;
     $scope.remsecs=0;
     $scope.remmillsecs=0;
+    var stop1;
+    var stop2;
     
-    $scope.setTimer=function(sec){
-        $scope.seconds=0;
-        $scope.remsecs=sec-1;
-        $scope.remmillsecs=(sec)*100;
-        if(sec>1) 
-            $interval(function(){$scope.remsecs--;},1000,sec-1);
-        else
-            $scope.remsecs=0;
-        $interval(function(){$scope.remmillsecs--;},10,(sec)*100);
+    $scope.setTime=function(sec){
+        if(sec!==0)
+        {  
+            //$scope.remsecs=sec-1;
+            $scope.remmillsecs=(sec)*100;
+            $scope.remsecs=$scope.remmillsecs/100;
+            $scope.seconds=0;
+        }
+        $scope.startTimer();
     };
+    
+    $scope.startTimer=function(){
+        //$scope.seconds=0;
+        //$scope.remsecs=sec-1;
+        //$scope.remmillsecs=(sec)*100;
+        
+        
+
+            /*stop1=$interval(function(){
+                if($scope.remsecs>0)
+                    $scope.remsecs--;
+            },1000);*/
+        
+        stop2=$interval(function(){
+            if($scope.remmillsecs>0)
+            {
+                $scope.remmillsecs--;
+                $scope.remsecs=$scope.remmillsecs/100;
+            }
+        },10);
+    };
+    
+    $scope.stopTimer=function(){
+        if(angular.isDefined(stop2))
+        {
+            //$interval.cancel(stop1);
+            $interval.cancel(stop2);
+            //stop1=undefined;
+            stop2=undefined;
+        }
+    };
+           
     
 }]);
